159317000fd38073a2d20565425c0b85f0d5ccbf
Thamidu Eshan <thamidueshan200@gmail.com>
2026-04-06 13:24:44 +0530

    Created player and enemy sprites with gravity and animation loop

--- index.js
+++ index.js
@@ -7,28 +7,61 @@ canvas.height = 576
 c.fillRect(0, 0, canvas.width, canvas.height)
 
 class sprite {
-    constructor(position) {
+    constructor({position, velocity}) {
     this.position = position
+    this.velocity = velocity
+    this.height = 150
     }
 
     draw(){
     c.fillStyle = 'red'
-    c.fillRect(this.position.x, this.position.y, 50, 150)   
+    c.fillRect(this.position.x, this.position.y, 50, this.height)   
+    }
+
+    update() {
+      this.draw() 
+      this.position.y += this.velocity.y
+
+      if(this.position.y + this.height + this.velocity.y >= canvas.height){
+        this.velocity.y = 0
+      }
     }
 }
 
 const player = new sprite({
+position: {
     x:0,
     y:0
+},
+velocity: {
+    x: 0,
+    y: 10
+}
 })
 
-player.draw()
+
 
 const enemy = new sprite({
+position: {
     x:400,
     y:100
+},
+velocity: {
+    x: 0,
+    y: 0
+}
 })
 
-enemy.draw()
+
 
 console.log(player)
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+}
+
+animate()
