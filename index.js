ab9271ae455005294bcd48d8578f337f310f5c38
Thamidu Eshan <thamidueshan200@gmail.com>
2026-04-08 23:12:51 +0530

    feat: add game timer and win condition logic

--- index.js
+++ index.js
@@ -136,6 +136,32 @@ function rectangularcollision({rectangle1, rectangle2 }) {
     )
 }
 
+function determineWinner(player, enemy) {
+            document.querySelector('#displayText').style.display = 'flex'
+if (player.health === enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Tie'
+        }else if (player.health > enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Player 1 wins'
+        }else if (player.health < enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Player 2 wins'
+        }
+}
+
+let timer = 60
+function decreaseTimer() { 
+    if (timer > 0) {
+        setTimeout(decreaseTimer, 1000)
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+        
+        if (timer === 0) {
+        determineWinner(player, enemy)
+        }
+    }
+}
+
+decreaseTimer()
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
@@ -182,6 +208,10 @@ function animate() {
         enemy.isAttacking = false
         player.health -= 20
         document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+    // end game based on health
+    if (enemy.health <= 0 || player.health <= 0){
+    determineWinner( player, enemy )
     }
 }
 
